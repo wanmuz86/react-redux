aa80a38f54e31431e32a92edd1111421300d7dc5
Wan Muzaffar <wanmuz@Wans-MacBook-Air.local>
2024-01-18 08:26:24 +0700

    add item

--- app/components/list.tsx
+++ app/components/list.tsx
@@ -1,13 +1,21 @@
+"use client"
 import React from 'react';
 import Row from './row';
+import { UseSelector, useSelector } from 'react-redux';
+
 export default function List() {
+  const todos = useSelector((state)=> state.todos.todos)
  return (
    <div>
        <h2>To do List</h2>
        <ul>
-           <li><Row/></li>
-           <li><Row/></li>
-           <li><Row/></li>
+          {
+            todos.map((todo)=>(
+              <li key={todo.id}>
+                {todo.text}
+              </li>
+            ))
+          }
        </ul>
    </div>
  )
